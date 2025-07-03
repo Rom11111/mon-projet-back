415264b40f58007dcd28d063975edab4572e3feb
Rom11111 <romain.1116@outlook.com>
2025-07-03 16:37:04 +0200

    compression des views

---
+++ src/main/java/org/romain/demo2/view/ProductViews.java
@@ -0,0 +1,7 @@
+package org.romain.demo2.view;
+
+public class ProductViews {
+    public interface Client {}
+    public interface Tech extends Client {}
+    public interface Admin extends Tech {}
+}
