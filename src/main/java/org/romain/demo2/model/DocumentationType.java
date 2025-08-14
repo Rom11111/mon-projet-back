c8fa498a75856e762cf0d6554cd8de1c8fe777af
Rom11111 <romain.1116@outlook.com>
2025-08-15 00:05:42 +0200

    Ajout du système de gestion de la documentation produit

---
+++ src/main/java/org/romain/demo2/model/DocumentationType.java
@@ -0,0 +1,8 @@
+package org.romain.demo2.model;
+
+public enum DocumentationType {
+    MANUAL,        // notice d'utilisation
+    TECH_DOC,      // documentation technique
+    VIDEO,         // vidéo (mp4, etc.)
+    IMAGE          // images produit (optionnel)
+}
